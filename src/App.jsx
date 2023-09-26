3fcdc50a074b1c88f057bfa074152094c922af3b
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-26 19:52:10 +0200

    usklađeno sa matejevom vjezbom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-//import "./App.css";
-import { useState } from "react";
-import DrugaKompa from "./components/DrugaKompa";
-import PrvaKompa from "./components/PrvaKompa";
-import Dohvati from "./Dohvati";
-import Login from "./components/Login"
+import './App.css'
+import DrugaKompa from './components/DrugaKompa'
+import PrvaKompa from './components/PrvaKompa'
+import Dohvati from './Dohvati'
+import Login from './components/Login'
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <>
-            {/* <PrvaKompa />
-            <DrugaKompa />
-            <Dohvati /> */}
-            <Login />
-        </>
-    );
+  return (
+    <>
+      {/* <PrvaKompa />
+      <DrugaKompa />
+      <Dohvati /> */}
+      <Login />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,37 +1,34 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import {useState, useEffect} from "react"
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    useEffect(() =>{
-        console.log(password)
-    },[password, email])
+    useEffect(() => {
+        console.log({password, email})
+    }, [password, email])
 
     return (
         <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
         </Form>
     );
-};
+}
 
 export default Login;
