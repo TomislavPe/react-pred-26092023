78b0a494e9a1721afe64883edcab59151434f05d
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-26 18:32:06 +0200

    prijmjer css stilova

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import DrugaKompa from "./components/DrugaKompa";
+import PrvaKompa from "./components/PrvaKompa";
+import Dohvati from "./Dohvati";
 
 function App() {
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <PrvaKompa />
+            <DrugaKompa />
+            <Dohvati />
+        </>
+    );
 }
 
-export default App
+export default App;
